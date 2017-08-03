607a9104e015d2ce67af981d8132024991b710de
Caitlin Murnick <caitlinmurnick@gmail.com>
2017-08-02 21:04:43 -0500

    added html

---
+++ main.js
@@ -0,0 +1,25 @@
+// document.getElementById("submit").addEventListener("click", "temp");
+
+
+// function toFahrenheit(temp) {
+// 	console.log(document.getElementById(temp));
+	
+	
+// }
+
+submit.addEventListener("click", console.log("temp"));
+
+
+
+
+
+// function determineConverter(submit)
+// 	document.getElementById("submit").addEventListener("click", displayDate);
+// Try it Yourself »
+
+
+// function toFahrenheit(temp) {
+// 	console.log(document.getElementById(temp));
+	
+	
+// }
