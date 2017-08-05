e4afe81623e56fab9f3e1ca111f0afee525ba1b4
Caitlin Murnick <caitlinmurnick@gmail.com>
2017-08-04 21:51:11 -0500

    adding java

--- main.js
+++ main.js
@@ -1,25 +1,58 @@
-// document.getElementById("submit").addEventListener("click", "temp");
 
 
-// function toFahrenheit(temp) {
-// 	console.log(document.getElementById(temp));
+
+function fahrenheit(ftemp) {
+	var ftemp = document.getElementById("temp").innerHTML;
+	ftemp = (ftemp * 1.8 ) + 32;
 	
-	
+}
+
+console.log(fahrenheit(ftemp));
+
+
+// function celcius(true) {
+// 	get div id of text area
+// 	do the math
 // }
 
-submit.addEventListener("click", console.log("temp"));
+function listen() {
+	run the listener event
+	submit.addEventListener("click", console.log("temp"));
 
+}
 
-
-
-
-// function determineConverter(submit)
-// 	document.getElementById("submit").addEventListener("click", displayDate);
-// Try it Yourself »
-
-
-// function toFahrenheit(temp) {
-// 	console.log(document.getElementById(temp));
-	
-	
+// if (fahrenheit(true)) {
+// to return checked property: radioObject.checked	
 // }
+	
+
+// To clear everything out at the end:
+
+// function uncheck() {
+// 	document.getElementById("red").checked = false;
+// }
+
+
+
+// function check() {
+//     document.getElementByClassName("celOrFahrChecked");
+// 	radioObject.checked = true|false;
+// 	return "celOrFahrChecked";
+
+// }
+
+// // var a = document.getElementById("cel").innerHTML;
+// // console.log(a);
+
+
+
+// // function determineConverter(submit)
+// // 	document.getElementById("submit").addEventListener("click", displayDate);
+// // Try it Yourself »
+
+
+// // function toFahrenheit(temp) {
+// // 	console.log(document.getElementById(temp));
+	
+	
+// // }
