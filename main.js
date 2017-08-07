08c3475a5b9e153fdf2759ed45bd5208dc99e646
Caitlin Murnick <caitlinmurnick@gmail.com>
2017-08-07 12:41:55 -0500

    finally figured it out

--- main.js
+++ main.js
@@ -7,31 +7,24 @@ var origNumberEntered = document.getElementById("originalTemp");
 // var whichChecked = celOrFahrChecked
 
 var element = document.getElementById("submitButton");
-element.addEventListener("click", tempInput );
-
-
+element.addEventListener("click", tempInput);
 
 function tempInput() {
 	var inputText = parseInt(origNumberEntered.value);
-	
-	// console.log("origNumberEntered:", inputText);
-	
 
 	if (document.getElementById("fahr").checked) {
-	var gatherInput = fahrenheit(inputText);
-	domWriter(gatherInput, "f");
+		var gatherInput = fahrenheit(inputText);
+		domWriter(gatherInput, "f");
 	}
-	
-	 else if (document.getElementById("celButton").checked) {
+	else if (document.getElementById("celButton").checked) {
 		// var inputText = gatherInput;
 		var gatherInput = celsius(inputText);
 		domWriter(gatherInput, "c");
-	} else {
+	}
+	else {
 		"You need to choose a conversion";
 	}
-
-
-	}
+}
 
 function fahrenheit(farTemp) {
 	var ftemp = (farTemp * 1.8 ) + 32;
@@ -48,67 +41,64 @@ function domWriter(gatherInput, t) {
 	var colorHolder;
 	if (t === "f") {
 		if (gatherInput > 90) {
-				colorHolder = "high";
-				console.log(colorHolder);
-				console.log(gatherInput)
-		} else if (gatherInput <32) {
-				colorHolder = "low";
-				console.log(colorHolder);
-				console.log(gatherInput);
-		} else {
-				colorHolder = "middle";
-				console.log(colorHolder);
-				console.log(gatherInput);
+			colorHolder = "high";
+			console.log(colorHolder);
+			console.log(gatherInput);
+		}
+		else if (gatherInput <32) {
+			colorHolder = "low";
+			console.log(colorHolder);
+			console.log(gatherInput);
+		}
+		else {
+			colorHolder = "middle";
+			console.log(colorHolder);
+			console.log(gatherInput);
 		}
 	} 
-
 	else {
-		if (t === "c") {
-			if (gatherInput > 32) {
-				colorHolder = "high";
-				console.log(colorHolder);
-				console.log(gatherInput)
-		} else if (gatherInput < 0) {
-				colorHolder = "low";
-				console.log(colorHolder);
-				console.log(gatherInput);
-		} else {
-				colorHolder = "middle";
-				console.log(colorHolder);
-				console.log(gatherInput);
+		if (gatherInput > 32) {
+			colorHolder = "high";
+			console.log(colorHolder);
+			console.log(gatherInput)
+		}
+		else if (gatherInput < 0) {
+			colorHolder = "low";
+			console.log(colorHolder);
+			console.log(gatherInput);
+		}
+		else {
+			colorHolder = "middle";
+			console.log(colorHolder);
+			console.log(gatherInput);
 		}
 	}
 	
-		var tempContainerColor = document.getElementById("poopOutput");
+	var tempDisplay = document.getElementById("tempDisplay");
+	tempDisplay.innerHTML = '<p>The converted temperature is ' + gatherInput + '</p>';
+	tempDisplay.className = colorHolder;
+}
+
+			// return domStringtoDom;
+			// document.write(domString);
+			// console.log(tempContainerColor);
+
+
+// 	} 	else if (colorHolder === "middle") {
+// 			tempContainerColor.innerHTML += domString;
+// 			domString +='<div id="poopOutput">';
+// 			domString += '<p id="colorGreen">' + 'The converted temperature is ' + colorHolder + '</p>';
+// 			domString += '</div>'
+// 			return tempContainerColor;
+// 			console.log(tempContainerColor);
+// 		} else {
+// 			tempContainerColor.innerHTML += domString;
+// 			domString +='<div id="poopOutput">';
+// 			domString += '<p id="colorBlue">' + 'The converted temperature is ' + colorHolder + '</p>';
+// 			domString += '</div>'
+// 			return tempContainerColor;
+// 			console.log(tempContainerColor);
 		
-		if (colorHolder === "high") {
-			tempContainerColor.innerHTML += domString;
-			domString = "";
-			// domString +='<div id="poopOutput">';
-			domString += '<p id="colorRed">' + 'The converted temperature is ' + colorHolder + '</p>';
-			domString += '</div>';
-			return domStringtoDom;
-			document.write(domString);
-			console.log(tempContainerColor);
-
-
-	} 	else if (colorHolder === "middle") {
-			tempContainerColor.innerHTML += domString;
-			domString +='<div id="poopOutput">';
-			domString += '<p id="colorGreen">' + 'The converted temperature is ' + colorHolder + '</p>';
-			domString += '</div>'
-			return tempContainerColor;
-			console.log(tempContainerColor);
-		} else {
-			tempContainerColor.innerHTML += domString;
-			domString +='<div id="poopOutput">';
-			domString += '<p id="colorBlue">' + 'The converted temperature is ' + colorHolder + '</p>';
-			domString += '</div>'
-			return tempContainerColor;
-			console.log(tempContainerColor);
-		}
-}
-}
 
 		
 	
